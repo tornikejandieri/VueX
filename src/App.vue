56acbb8c726507df27e5944694f7f65bd3c0c804
Tornike Jandieri <108745129+tornikejandieri@users.noreply.github.com>
2022-09-28 19:07:28 +0400

    control mutations

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <base-container title="Vuex">
     <the-counter></the-counter>
-    <button @click="addOne">Add 1</button>
+    <button @click="addOne">Add 10</button>
     <change-counter></change-counter>
   </base-container>
 </template>
@@ -20,7 +20,10 @@ export default {
 
   methods: {
     addOne() {
-      this.$store.commit('increment');
+      this.$store.commit({
+        type: 'increase',
+        value: 10,
+      });
     },
   },
 };
